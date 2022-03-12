5ec52296c0fddb4d46371ce30f5e806e15987b50
Bikram Rumba <rumba@gmail.com>
2022-03-12 17:34:33 -0600

    Comparison operators in JS

--- ComparisonOperators.js
+++ ComparisonOperators.js
@@ -1,4 +1,4 @@
-// JS comparison operators
+/* // JS comparison operators
 // 1. Relational --> Compare values in relation to one another
 // 2. Abstract (or loose) --> Check to see if the values are the same
 // 3. Strict --> Check to see if the values are the same AND the same type
@@ -21,5 +21,36 @@ console.log(x <= 10, x <= 7);
 //  == equal to
 console.log(x == 7, x == 6);
 
-//  != equal to
+//  !=  not equal to
 console.log(x != 7);
+
+// 3. Strict
+// ==== strict equality
+let y = '7';
+console.log(x === 7, x === y); // x===y is false because y is a string.
+
+// !== strict not equal to
+console.log(x !== y);
+
+// comparison in Strings
+console.log('A' < 'Z');
+
+let a = 'Time';
+let b = 'Timing';
+
+console.log(a > b, 'e' > 'i');
+ */
+
+let a = 10;
+let b = 15;
+console.log(a > b);
+
+console.log(a == '10'); // it willl return true because value is 10
+
+//strict equality with data type and value
+console.log(a === '10'); // it will return false this time.
+
+//  if x is equal to '5' and y is equal to 6, what would be the result of x !==y?
+let x = '5';
+let y = 6;
+console.log(x !== y);
