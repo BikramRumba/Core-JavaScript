8ad1049ae1a0d509ead71eb6f21ab236670e532e
Bikram Rumba <rumba@gmail.com>
2022-03-13 01:08:20 -0600

    if else checked

---
+++ ControlFlow.js
@@ -0,0 +1,24 @@
+/* By default our JavaScript code is executed from a top to bottom, line by line however we can change
+this with Control Flow. Control Flow allows our program to make decision about what code is executed and when.
+JS control flow is all about how we control the flow of our code this is done by using javascript's conditional statements
+and loops, and finally using control flow makes our projects more powerful, more dynamic and more interactive
+*/
+
+// Conditional Statements
+/*  Conditional Statements are basically checks to
+see if a certain condition is either true or flase.
+If the condition is true then run code A, if it's false then run B
+1. if statements
+2. switch
+3. ternary
+*/
+
+// JavaScript Loops
+/*  Loops allow us to perform repetitive tasks with less code. For example using loops we can
+iterate over an array and perform some sort of action on each piece of data in the array, or
+we can use a loop to compile a list of all the items. 
+1. for loops
+2. do/while loop
+3. for..in
+4.for..of
+*/
---
+++ ifElse.js
@@ -0,0 +1,47 @@
+// if(condition){
+// // code to be run if condition is true
+// }
+// let game = 'Mario';
+// if (game === 'Sonic') {
+// 	console.log('The condition is true');
+// } else {
+// 	//code to run if the condition is false
+// 	console.log('the condition is false');
+// }
+
+// let roll = 3;
+
+// if (roll === 4) {
+// 	console.log('You rolled a four');
+// } else if (roll === 5) {
+// 	console.log('You rolled a five');
+// } else if (roll === 6) {
+// 	console.log('you rolled a six');
+// } else {
+// 	console.log('You rolled less than a four');
+// }
+
+// TASKS
+let num = 10 + 2;
+if (num > 2 && num < 20) {
+	console.log('TRUE');
+} else {
+	console.log('FALSE');
+}
+
+let user = 'employee';
+
+if (user === 'guest') {
+	console.log('Login Denied');
+} else if (user === ' employee') {
+	console.log('Successfully Logged in');
+}
+
+let myName = 'Bikram';
+if (myName.length > 5) {
+	alert('More than 5 letters');
+} else if (myName.length === 5) {
+	alert('Exactly 5 letters');
+} else {
+	alert('Less than 5 letters');
+}
