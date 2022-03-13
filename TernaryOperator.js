e6ba6737f4ea457f2ae1cd6d968e461d4587b779
Bikram Rumba <rumba@gmail.com>
2022-03-13 13:19:35 -0500

    ternary operators

---
+++ TernaryOperator.js
@@ -0,0 +1,33 @@
+/* 
+    The Ternary Operator provides us with an easier way to write an if else statement.
+     The word ternay means 'composed of three parts'
+     statement ? true : false;
+*/
+
+// let age = 18;
+// if (age >= 18) {
+// 	console.log('You can vote');
+// } else {
+// 	console.log('Sorry, you cannot vote');
+// }
+
+// let message = age >= 18 ? 'You can vote' : 'Sorry, You cant vote';
+// console.log(message);
+
+// let password = 'redruby123';
+// let message = password === 'redruby123' ? 'Success' : 'Fail';
+// console.log(message);
+
+// DOM Document Object Model.
+// let body = document.querySelectorAll('body');
+// let choice = 'dark';
+// choice === 'dark' ? body.classList.add('darkMode') : null;
+
+// TASKS
+
+let message = 1 > 10 ? 'Condition is True' : 'Condition is false';
+console.log(message);
+
+let time = '12:00';
+let greeting = time < '12:00' ? 'Good Morning' : 'Good Evening';
+console.log(greeting);
